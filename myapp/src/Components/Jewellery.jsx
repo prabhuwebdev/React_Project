7acdbe922fd8057d3c00834245f414eeda3ba82d
prabhuwebdev <h.prabhu2dev2@gmail.com>
2024-12-10 12:43:12 +0530

    second change

--- myapp/src/Components/Jewellery.jsx
+++ myapp/src/Components/Jewellery.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 // import { useEffect,useState } from 'react'
-import Card from 'react-bootstrap/card'
+import Card from "react-bootstrap/Card"
 import '../CssFolder/jewelery.css'
 import Usefetch from './Usefetch'
 import { useNavigate } from 'react-router-dom'
